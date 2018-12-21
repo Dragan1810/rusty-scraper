be768b99dd76838f5e16486ef8a9879e130eb957
dragan1810 <d.novakovic1810@yahoo.com>
2018-12-21 21:41:04 +0100

    fantocini and headless geckodriver working

--- src/main.rs
+++ src/main.rs
@@ -28,38 +28,45 @@ fn main() {
 
     let res = reqwest::get(url).unwrap();
     let document = Document::from_read(res).unwrap();
-
+    /*
     for node in document.find(Name("h3").descendant(Name("span"))) {
         println!("{:#?}", node.text());
     }
+    */
 
     let server = c
         .map_err(|e| {
             unimplemented!("failed to connect to WebDriver: {:?}", e)
         })
-        .and_then(|c| {
-            // first, go to the Wikipedia page for Foobar
-            c.goto("https://en.wikipedia.org/wiki/Foobar")
-        })
-        .and_then(|mut c| c.current_url().map(move |url| (c, url)))
-        .and_then(|(mut c, url)| {
-            assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foobar");
-            // click "Foo (disambiguation)"
-            c.find(Locator::Css(".mw-disambig"))
-        })
-        .and_then(|e| e.click())
+        .and_then(|c| c.goto("https://www.wikipedia.org/"))
         .and_then(|mut c| {
-            // click "Foo Lake"
-            c.find(Locator::LinkText("Foo Lake"))
+        // find the source for the Wikipedia globe
+        c.find(Locator::Css("img.central-featured-logo"))
         })
-        .and_then(|e| {
-            e.click()
-            })
-        .and_then(|mut c| c.current_url())
-        .and_then(|url| {
-            assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foo_Lake");
-            Ok(())
+        .and_then(|mut img| {
+            img.attr("src").map(move |src| (img, src.expect("image should have a src")))
         })
+        .and_then(move |(img, src)| {
+        // now build a raw HTTP client request (which also has all current cookies)
+            img.client().raw_client_for(fantoccini::Method::GET, &src)
+        })
+        .and_then(|raw| {
+            use futures::Stream;
+        // we then read out the image bytes
+            raw.into_body().map_err(fantoccini::error::CmdError::from).fold(
+                Vec::new(),
+                |mut pixels, chunk| {
+                pixels.extend(&*chunk);
+                futures::future::ok::<Vec<u8>, fantoccini::error::CmdError>(pixels)
+                },
+            )
+    })
+    .and_then(|pixels| {
+        // and voilla, we now have the bytes for the Wikipedia logo!
+        assert!(pixels.len() > 0);
+        println!("Wikipedia logo is {}b", pixels.len());
+        Ok(())
+    })
         .map_err(|e| {
             panic!("a WebDriver command failed: {:?}", e);
         });
