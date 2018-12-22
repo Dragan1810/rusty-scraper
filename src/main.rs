98ede4f5ab28dffac9e9f8e693a1083a70e600e7
dragan1810 <d.novakovic1810@yahoo.com>
2018-12-22 22:01:22 +0100

    clicking all matches to trigger ajax content

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use select::predicate::{Predicate, Attr, Class, Name};
 use fantoccini::{Client, Locator};
 use futures::future::Future;
 
+pub static scores_way: &str = "http://www.scoresway.com/?sport=home&page=matches&date=2018-12-17";
+
 fn make_url(_stuff: &str) -> String {
     let sport = "soccer";
     let page = "matches";
@@ -19,11 +21,10 @@ fn make_url(_stuff: &str) -> String {
 }
 
 fn main() {
-    let scores_way = "http://www.scoresway.com/?sport=home&page=matches&date=2018-12-17";
     let _bin = "http://httpbin.org/";
     let _url1 = "http://www.blankwebsite.com/";
 
-    let url: &str = &make_url(scores_way)[..];
+    let url: &str = &make_url(scores_way);
     let c = Client::new("http://localhost:4444");
 
     let res = reqwest::get(url).unwrap();
@@ -38,35 +39,18 @@ fn main() {
         .map_err(|e| {
             unimplemented!("failed to connect to WebDriver: {:?}", e)
         })
-        .and_then(|c| c.goto("https://www.wikipedia.org/"))
+        .and_then(move |c| c.goto(&make_url(scores_way)))
         .and_then(|mut c| {
-        // find the source for the Wikipedia globe
-        c.find(Locator::Css("img.central-featured-logo"))
+            c.find_all(Locator::Css(".group-head.clickable"))
+
         })
-        .and_then(|mut img| {
-            img.attr("src").map(move |src| (img, src.expect("image should have a src")))
+        .and_then(|mut elements| {
+            for el in elements {
+                el.click();
+            }
+            //img.attr("src").map(move |src| (img, src.expect("image should have a src")))
+            Ok(())
         })
-        .and_then(move |(img, src)| {
-        // now build a raw HTTP client request (which also has all current cookies)
-            img.client().raw_client_for(fantoccini::Method::GET, &src)
-        })
-        .and_then(|raw| {
-            use futures::Stream;
-        // we then read out the image bytes
-            raw.into_body().map_err(fantoccini::error::CmdError::from).fold(
-                Vec::new(),
-                |mut pixels, chunk| {
-                pixels.extend(&*chunk);
-                futures::future::ok::<Vec<u8>, fantoccini::error::CmdError>(pixels)
-                },
-            )
-    })
-    .and_then(|pixels| {
-        // and voilla, we now have the bytes for the Wikipedia logo!
-        assert!(pixels.len() > 0);
-        println!("Wikipedia logo is {}b", pixels.len());
-        Ok(())
-    })
         .map_err(|e| {
             panic!("a WebDriver command failed: {:?}", e);
         });
